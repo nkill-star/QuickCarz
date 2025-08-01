c9ca94633b1f71eff0ce8e462bb84fe5ee00bf43
nikhil.s.richie@gmail.com <nikhil.s.richie@gmail.com>
2025-08-02 00:54:03 +0530

    updation

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -66,7 +66,7 @@ const Hero = () => {
             initial={{y:100,opacity:0}}
             animate={{y:0, opacity:1}}
             transition={{duration:0.8, delay:0.6}}
-            src={assets.main_car} alt="car" className='max-h-74' />
+            src={assets.main_car} alt="car" className='max-h-100' />
 
         </motion.div>
     )
